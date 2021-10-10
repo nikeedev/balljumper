995aabfe6220fd2dca297ecdc72897029ded8eb5
Nikegamerjjjj <myftr12394@gmail.com>
2021-10-10 19:49:16 +0200

    Updates

--- src/ball.js
+++ src/ball.js
@@ -1,14 +1,41 @@
 export default class Ball {
-    constructor() {
+    constructor(game) {
         this.image = document.getElementById("img_ball");
+        
+        
+        this.gameWidth = game.gameWidth;
+        this.gameHeight = game.gameHeight;
+
+
+        this.postion = {x: 10, y: 10};
+
+        this.speed = {x: 4, y: 2};
+
+        this.size = 16;
     }
 
     draw(ctx) {
-        ctx.drawImage(this.image, 10, 10, 16, 16);
+        ctx.drawImage(
+            this.image,
+            this.postion.x, 
+            this.postion.y, 
+            this.size, 
+            this.size
+        );
     }
 
-    update() {
+    update(deltaTime) {
+        this.postion.x += this.speed.x;
+        this.postion.y += this.speed.y;
 
+
+        if(this.postion.x + this.size > this.gameWidth || this.postion.x < 0) {
+            this.speed.x = -this.speed.x;
+        }
+
+        if(this.postion.y + this.size > this.gameHeight || this.postion.y < 0) {
+            this.speed.y = -this.speed.y;
+        }
     }   
 
 }
--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
-import Paddle from "./paddle.js";
-import InputHandler from "./input.js";
-import Ball from "./ball.js";
+
+import Game from "./game.js";
 
 
 let canvas = document.getElementById("gameScreen");
@@ -10,38 +9,29 @@ let ctx = canvas.getContext("2d");
 const GAME_WIDTH = 600;
 const GAME_HEIGHT = 500;
 
-ctx.clearRect(20, 20, 600, 500);
 
-let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
-let ball = new Ball();
+let game = new Game(GAME_WIDTH, GAME_HEIGHT);
+game.start();
 
-
-new InputHandler(paddle);
-
-paddle.draw(ctx);
 
 
 let lastTime = 0;
-
-
-
-
 function gameLoop(timestamp) {
     let deltaTime = timestamp - lastTime;
     lastTime = timestamp;
 
 
-    ctx.clearRect(0, 0, 600, 500);
-    paddle.update(deltaTime);
-    paddle.draw(ctx);
+    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
     
-    ball.draw(ctx);
+    
+    game.update(deltaTime);
+    game.draw(ctx);
 
 
     requestAnimationFrame(gameLoop);
 }
 
 
-gameLoop();
+requestAnimationFrame(gameLoop);
 
 
